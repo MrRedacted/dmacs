6649f0b63d86c3a829d002518707e573852e71f3
Darius Brown <59292656+MrRedacted@users.noreply.github.com>
2024-09-10 08:24:16 -0400

    Changed "tsserver" to "ts_ls" in lspconfig

--- lua/config/lazy/lspconfig.lua
+++ lua/config/lazy/lspconfig.lua
@@ -91,8 +91,8 @@ return {
           })
         end,
 
-        ["tsserver"] = function()
-          lspconfig.tsserver.setup({
+        ["ts_ls"] = function()
+          lspconfig.ts_ls.setup({
             capabilities = capabilities,
             init_options = {
               plugins = {
