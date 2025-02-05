70093f8de1a4845bc83258a823bec126e706728e
Darius Brown <59292656+MrRedacted@users.noreply.github.com>
2025-02-05 14:34:24 -0500

    switched from ruby-lsp to solargraph

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 cd ~/.local/share/nvim/mason/packages/vue-language-server || exit
 npm install @vue/typescript-plugin
 
-gem install ruby-lsp rubocop rubocop-rails erb-formatter erb_lint
+# gem install group for ruby-lsp setup
+#gem install ruby-lsp rubocop rubocop-rails erb-formatter erb_lint
+
+# gem install group for solargraph setup
+gem install solargraph solargraph-rails rubocop rubocop-rails erb-formatter erb_lint
