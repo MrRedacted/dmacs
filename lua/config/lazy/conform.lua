03b187b3b7973f51710737f1b209ff7259196c62
Darius Brown <59292656+MrRedacted@users.noreply.github.com>
2025-02-05 13:02:11 -0500

    fixed issue with rubocop caching breaking the linter/formatter

--- lua/config/lazy/conform.lua
+++ lua/config/lazy/conform.lua
@@ -10,7 +10,7 @@ return {
 
         zig = { "zigfmt" },
 
-        ruby = { "rubocop" },
+        ruby = { lsp_format = "fallback" },
         eruby = { "erb_format" },
 
         sh = { "shfmt" },
